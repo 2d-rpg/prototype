aa99bf8750c3dc32376537b2b4b2eb47e0530cd3
nyatadecocoa <yoshii.y.ad@m.titech.ac.jp>
2019-04-26 22:21:13 +0900

    modify to use test_map(.gif) & to run on Mac

---
+++ src/texture/texture_manager.cpp
@@ -0,0 +1,27 @@
+#include <texture/texture_manager.h>
+#include <shader/shader.h>
+
+TextureManager::TextureManager(const GLchar* vertex_shader, const GLchar* fragment_shader)
+    : shader(vertex_shader, fragment_shader) {
+    textures = std::map<std::string, Texture2D>();
+}
+
+TextureManager::~TextureManager() {}
+
+void TextureManager::load(const char* file_path, std::string name) {
+    textures[name] = Texture2D();
+    textures[name].generate(file_path);
+};
+
+Shader TextureManager::get_shader() {
+    return this->shader;
+}
+
+Texture2D TextureManager::get_texture(std::string key) {
+    std::map<std::string, Texture2D>::iterator ret = this->textures.find(key);
+
+    if (ret == this->textures.end()) {
+        std::cerr << "could not find texture with that key. (TextureManager)" << std::endl;
+    }
+    return ret->second;
+}
