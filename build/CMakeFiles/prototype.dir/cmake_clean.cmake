94978d563810b3174d7fce34d27458953a9274e9
nyatadecocoa <yoshii.y.ad@m.titech.ac.jp>
2019-04-26 22:15:17 +0900

    modify to use test_map(.gif) & to run on Mac

---
+++ build/CMakeFiles/prototype.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prototype.dir/src/renderer/map.cpp.o"
+  "CMakeFiles/prototype.dir/src/shader/shader.cpp.o"
+  "CMakeFiles/prototype.dir/src/texture/texture_2D.cpp.o"
+  "CMakeFiles/prototype.dir/src/texture/texture_manager.cpp.o"
+  "CMakeFiles/prototype.dir/src/glad.c.o"
+  "CMakeFiles/prototype.dir/src/stb_image.cpp.o"
+  "CMakeFiles/prototype.dir/src/utils.cpp.o"
+  "CMakeFiles/prototype.dir/src/main.cpp.o"
+  "prototype.pdb"
+  "prototype"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/prototype.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/prototype_libs.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libprototype_libs.a"
+)
